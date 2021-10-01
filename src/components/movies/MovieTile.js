e2759a54da7a86bc5db26c826b6fb3545243b9b2
DIANA MARTYN <dianamartin790@gmail.com>
2021-10-01 16:32:37 +0300

    styling fav btn

--- src/components/movies/MovieTile.js
+++ src/components/movies/MovieTile.js
@@ -18,7 +18,7 @@ const MovieTile = ({ image, name, rating, id, favourite }) => {
     }
 
     return <div className="movie">
-        <button className={`like-btn ${favourite ? 'liked' : 'notLiked'}`} onClick={sendDataToCollection}>♥</button>
+        <button className={`like-btn ${!user ? 'disable-btn' : ''} ${favourite ? 'liked' : 'notLiked'}`} onClick={sendDataToCollection}>♥</button>
         <img onClick={() => history.push('/movies/' + id)} src={image?.medium ? image?.medium : 'https://images.unsplash.com/photo-1518676590629-3dcbd9c5a5c9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60'} alt={name} />
         < div className="movie-info">
             <h3>{name}</h3>
